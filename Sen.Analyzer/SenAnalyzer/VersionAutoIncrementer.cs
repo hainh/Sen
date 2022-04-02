cbd301ae8bcf4358b0e0927c4d4b2e3b1011d8dd
hainh <hainh.vn@gmail.com>
2022-04-02 10:16:41 +0700

    * Fix analyzer

---
+++ Sen.Analyzer/SenAnalyzer/VersionAutoIncrementer.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.751.1919")]
+[assembly: AssemblyCompany("SenAnalyzer")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.751.1919")]
+[assembly: AssemblyInformationalVersion("1.0.751.1919")]
+[assembly: AssemblyProduct("SenAnalyzer")]
+[assembly: AssemblyTitle("SenAnalyzer")]
+
